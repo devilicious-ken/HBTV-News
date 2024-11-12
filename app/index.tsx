1cfa01d42ffed78af3fe698a1107b613361e7b5e
devilicious-ken <bacoy.kent.brian.gmail.com>
2024-11-12 14:37:17 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Image, StyleSheet, Platform, SafeAreaView, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={{margin:'auto'}}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+}
+
